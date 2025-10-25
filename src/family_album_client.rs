422e26e45f663716771af1999237eb8a866c0118
Thomas Holmes <thomasholmes@outlook.com>
2025-10-25 10:31:46 +0100

    Updated useragent header.

--- src/family_album_client.rs
+++ src/family_album_client.rs
@@ -13,7 +13,7 @@ use reqwest::{Client, Error, header};
 use crate::model::{Mediafile, Root};
 
 const USER_AGENT: &str =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0";
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:144.0) Gecko/20100101 Firefox/144.0";
 pub struct FamilyAlbumClient {
     base_address: String,
     password: String,
